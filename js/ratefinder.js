f7f42ded5e0ccd4cfcbf8950cea244de4ee51e9e
Radosław Nowacki <nowacki@MacBook-Pro-Radosaw.local>
2016-02-02 15:19:34 +0100

    first commit

---
+++ js/ratefinder.js
@@ -0,0 +1,25 @@
+let url = "rates.json";
+
+
+// using Promises
+//fetch(url)
+//    .then(response => response.json())
+//    .then(rates => {
+//        let html = '';
+//        rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
+//        document.getElementById("rates").innerHTML = html;
+//    })
+//    .catch(e => console.log(e));
+
+
+// creating Promises
+
+import * as service from './rate-service-mock';
+
+service.findAll()
+    .then(rates => {
+        let html = '';
+        rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
+        document.getElementById("rates").innerHTML = html;
+    })
+    .catch(e => console.log(e));
